0ffb7e2ab9309cf7eff8c68671609c5061624666
Sean Dague <sean@dague.net>
2016-02-15 09:38:32 -0500

    get a message sending test working

--- tests/test_mqtt_real.py
+++ tests/test_mqtt_real.py
@@ -9,6 +9,7 @@ Tests for `arwn` module.
 """
 
 import sys
+import time
 import unittest
 
 import paho.mqtt.client as mqtt
@@ -20,14 +21,37 @@ from . import arwn_fixtures
 class TestMqttSpawn(testtools.TestCase):
 
     def test_mqtt_fixture(self):
+        self.connected = False
+        self.received = ""
+        self.done = False
+
+        def on_connect(client, userdata, flags, rc):
+            client.subscribe("foo/#")
+            self.connected = True
+
+        def on_message(client, userdata, message):
+            self.received = message
+            self.done = True
+
         try:
             mos = arwn_fixtures.MosquittoReal()
             self.useFixture(mos)
         except arwn_fixtures.MosquittoSetupFail:
             self.skipTest("Can't start mosquitto")
+
         client = mqtt.Client()
+        client.on_connect = on_connect
+        client.on_message = on_message
         client.connect("localhost", mos.port)
-        client.publish("foo/start", "foo")
+        client.loop_start()
+        client.publish("foo/start", "foo", retain=True)
+        while not self.done:
+            # forces a flush of all messages
+            time.sleep(0.1)
+            client.loop_read()
+        self.assertTrue(self.connected, "Did not seem to connect")
+        self.assertEqual("foo", self.received.payload)
+        self.assertEqual("foo/start", self.received.topic)
 
 
 if __name__ == '__main__':
