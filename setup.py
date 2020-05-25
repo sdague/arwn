3f8d2af34107172dc4d0c3465fc782819a1cf8b1
Sean Dague <sean@dague.net>
2020-05-25 10:46:12 -0400

    Bump version: 1.2.0 → 2.0.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ test_requirements.extend(requirements)
 
 setup(
     name='arwn',
-    version='1.2.0',
+    version='2.0.0',
     description="Collect 433Mhz weather sensor data and publish to mqtt",
     long_description=readme + '\n\n' + history,
     author="Sean Dague",
