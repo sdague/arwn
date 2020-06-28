081322d945b31de6989182c42e68fa6aa3236308
Andrew Fahrenholtz <andrew.fahrenholtz+github@gmail.com>
2020-06-28 11:12:44 -0500

    debug time

--- arwn/sensor/sensor_factory.py
+++ arwn/sensor/sensor_factory.py
@@ -1,4 +1,8 @@
+import logging
+
 from arwn.sensor.acurite5n1 import Acurite5n1
+
+logger = logging.getLogger(__name__)
 
 class SensorFactory(object):
     @staticmethod
@@ -14,6 +18,7 @@ class SensorFactory(object):
     @staticmethod
     def create_acurite_5n1(data):
         if Acurite5n1.parse_time(data["time"]) != Acurite5n1.previous_time:
+            logger.debug("time (%s) is not equal previous time (%s)" % (data["time"], Acurite5n1.previous_time))
             return Acurite5n1(data)
         else:
             return None
