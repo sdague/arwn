41938ccc1ff2ee275f607f15356969806c29db85
Sean Dague <sean@dague.net>
2026-03-08 05:58:40 -0400

    Modernize Python packaging to PEP 517/518/621 standards
    - Add pyproject.toml with PEP 621 metadata
    - Update setup.py to minimal shim for backward compatibility
    - Simplify setup.cfg to tool-specific configurations only
    - Update requirements files with modern versions
    - Add PACKAGING.md documentation
    - Support Python 3.8-3.12
    - Use SPDX license identifier (Apache-2.0)
    - Configure modern tools (black, isort, mypy, pytest)
    - Successfully builds wheel and sdist
    - Tests pass with new configuration

--- setup.py
+++ setup.py
@@ -1,61 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+Minimal setup.py for backward compatibility.
+All configuration is now in pyproject.toml (PEP 621).
+"""
 
+from setuptools import setup
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-requirements = [
-    'pyyaml>=5.1',
-    'python-daemon',
-    'paho-mqtt>=1.1',
-    'pyserial>=3',
-    'pid>=2'
-]
-
-test_requirements = [
-    'mock',
-    'fixtures'
-    # TODO: put package test requirements here
-]
-
-test_requirements.extend(requirements)
-
-setup(
-    name='arwn',
-    version='2.0.0',
-    description="Collect 433Mhz weather sensor data and publish to mqtt",
-    long_description=readme,
-    author="Sean Dague",
-    author_email='sean@dague.net',
-    url='https://github.com/sdague/arwn',
-    packages=['arwn', 'arwn.cmd', 'arwn.vendor', 'arwn.vendor.RFXtrx'],
-    package_dir={
-        'arwn': 'arwn',
-    },
-    scripts=['bin/arwn-collect'],
-    include_package_data=True,
-    install_requires=requirements,
-    license="Apache 2",
-    zip_safe=False,
-    keywords='arwn',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
-    test_suite='tests',
-    tests_require=test_requirements
-)
+setup()
